9369e6cfba4dc43daf45c2fa94fd88c5c1633ff7
Emad Rahimi <121079771+erscript@users.noreply.github.com>
2024-01-05 09:07:37 +0330

    Add movie model and controller

---
+++ db/migrate/20240105053504_create_movies.rb
@@ -0,0 +1,21 @@
+class CreateMovies < ActiveRecord::Migration[7.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.string :tagline
+      t.string :age_rating
+      t.integer :duration
+      t.date :release_date
+      t.integer :status
+      t.integer :release_status
+      t.string :language
+      t.decimal :budget
+      t.decimal :revenue
+      t.text :overview
+      t.string :image_filename
+      t.string :gradient_color
+
+      t.timestamps
+    end
+  end
+end
