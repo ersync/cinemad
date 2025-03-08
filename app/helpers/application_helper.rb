63b52cd90cee052ca4ac3e7a91bfde7ae0ff3daa
Emad Rahimi <121079771+ersync@users.noreply.github.com>
2025-03-08 10:39:44 +0330

    refactor(backend): remove deprecated view helpers

--- app/helpers/application_helper.rb
+++
@@ -1,9 +0,0 @@
-module ApplicationHelper
-  require 'color' # make sure 'color' gem is in your Gemfile
-
-  def darken_color(hex_color, amount = 0.1)
-    color = Color::RGB.by_hex(hex_color)
-    darkened = color.darken_by(amount * 100)
-    "#" + darkened.hex
-  end
-end
