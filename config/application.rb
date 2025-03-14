45564b73317c519695516ae5e233231cece37b5f
Emad Rahimi <121079771+ersync@users.noreply.github.com>
2025-03-14 12:16:00 +0330

    refactor: rename project from TMDB-clone to Cinemad
    The new name combines "Cinema" with "Emad" for a personalized touch.

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ require "action_cable/engine"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module TmdbClone
+module Cinemad
   class Application < Rails::Application
 
     config.after_initialize do
